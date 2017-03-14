24184a52848cfdaf5bd1fd5104990b8cb9044105
Slawcio <slawomir.rudawski@gmail.com>
2017-03-14 20:51:35 +0100

    aplikacja mobilna połączona z serwerem

--- HomeLibraryMobile/index.android.js
+++ HomeLibraryMobile/index.android.js
@@ -9,20 +9,57 @@ import {
   AppRegistry,
   StyleSheet,
   Text,
-  View
+  View,
+  ToastAndroid
 } from 'react-native';
+import signalr from 'react-native-signalr';
 
 export default class HomeLibraryMobile extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      signalRconnected: false
+      signalRconnected: false,
+      libraryState: ''
     }
   }
-  
+
+  componentDidMount() {
+    const connection = signalr.hubConnection('http://192.168.0.19:57123');
+    connection.logging = true;
+
+    const proxy = connection.createHubProxy('library');
+    proxy.on('updateLibraryState', (libraryState) => {
+      this.setState({ libraryState: libraryState });
+    });
+
+    connection.start().done(() => {
+      ToastAndroid.show('signalr connected', ToastAndroid.SHORT);
+      this.setState({ signalRconnected: true })
+      proxy.invoke('getLibraryState', 'Mobile')
+        .fail(() => {
+          ToastAndroid.show('getLibraryState fail', ToastAndroid.SHORT);
+        });
+    }).fail(() => {
+      ToastAndroid.show('signalr connection failed', ToastAndroid.SHORT);
+    });
+
+    connection.error((error) => {
+      const errorMessage = error.message;
+      let detailedError = '';
+      if (error.source && error.source._response) {
+        detailedError = error.source._response;
+      }
+      ToastAndroid.show('signalr connection failed'+ errorMessage, ToastAndroid.SHORT);
+    });
+  }
+
   _renderHomeView() {
     if (this.state.signalRconnected) {
-      return (<Text>Połączono z serwerem :)</Text>)
+      return (
+        <View>
+          <Text>Połączono z serwerem :)</Text>
+          <Text>Stan biblioteki: { this.state.libraryState }</Text>
+        </View>)
     } else {
       return (<Text>Brak połączenia z serwerem :(</Text>)
     }
