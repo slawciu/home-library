72f464d6a14c60746b8af7668cd0f7474f666f42
Slawcio <slawomir.rudawski@gmail.com>
2017-03-14 20:16:22 +0100

    w drodze do połączenia z serwerem

--- HomeLibraryMobile/index.android.js
+++ HomeLibraryMobile/index.android.js
@@ -13,19 +13,26 @@ import {
 } from 'react-native';
 
 export default class HomeLibraryMobile extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      signalRconnected: false
+    }
+  }
+  
+  _renderHomeView() {
+    if (this.state.signalRconnected) {
+      return (<Text>Połączono z serwerem :)</Text>)
+    } else {
+      return (<Text>Brak połączenia z serwerem :(</Text>)
+    }
+  }
+  
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Double tap R on your keyboard to reload,{'\n'}
-          Shake or press menu button for dev menu
-        </Text>
+        <Text>Domowa Biblioteka</Text>
+        { this._renderHomeView() }
       </View>
     );
   }
