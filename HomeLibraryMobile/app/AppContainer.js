5ce365b027cf17201e979956c4b564d0cc20371e
Sławomir Rudawski <slawomir.rudawski@gmail.com>
2017-03-16 18:02:59 +0100

    użycie homelibrary z redux

--- HomeLibraryMobile/app/AppContainer.js
+++ HomeLibraryMobile/app/AppContainer.js
@@ -1,25 +1,12 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { ActionCreators } from '../actions'
-import { View, Text, TouchableHighlight } from 'react-native'
 import { bindActionCreators } from 'redux'
-import { HomeLibraryMobile } from './HomeLibraryMobile'
+import  HomeLibraryMobile  from './HomeLibraryMobile'
 
 class AppContainer extends Component {
-    _selectBook () {
-        this.props.selectBook();
-    }
-
     render() {
-        return (
-            <View>
-                <Text>
-                    Zaznaczona książka: { this.props.selectedBook } 
-                </Text>
-                <TouchableHighlight onPress={() => this._selectBook()}>
-                    <Text>Select!</Text>
-                </TouchableHighlight>
-            </View> );
+        return <HomeLibraryMobile {...this.props} />;
     }
 }
  
@@ -27,8 +14,4 @@ function mapDispatchToProps(dispatch) {
     return bindActionCreators(ActionCreators, dispatch);
 }
 
-export default connect((state) => { 
-    return {
-        selectedBook: state.selectedBook
-    }
- }, mapDispatchToProps)(AppContainer);
+export default connect(() => { return {} }, mapDispatchToProps)(AppContainer);
