3ea8aa063a3293b546c1a6071010a37c6575049a
Slawcio <slawomir.rudawski@gmail.com>
2017-04-06 16:56:36 +0200

    wyswietlenie informacji o zeskanowanej książce

--- HomeLibraryMobile/actions/books.js
+++ HomeLibraryMobile/actions/books.js
@@ -23,7 +23,7 @@ export function updateLibraryState({ libraryState }) {
     }
 }
 
-export function newBookInfoRceived({bookInfo}) {
+export function newBookInfoReceived(bookInfo) {
     return {
         type: types.NEW_BOOK_RECEIVED,
         newBook: bookInfo
@@ -66,7 +66,7 @@ export function connectToSignalR() {
         });
 
         proxy.on('newBookInfo', (newBook) => {
-            dispatch(newBookInfoRceived({newBook}))
+            dispatch(newBookInfoReceived(newBook))
         });
 
         connection.start()
